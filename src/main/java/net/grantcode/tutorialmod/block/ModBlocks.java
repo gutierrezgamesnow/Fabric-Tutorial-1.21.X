c13c8169604f690ae1f5cd64c4f62ebbd44930a9
gutierrezgamesnow <gutierrez.games.now@gmail.com>
2026-09-24 11:18:16 -0500

    started to add a block

--- src/main/java/net/grantcode/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/grantcode/tutorialmod/block/ModBlocks.java
@@ -14,7 +14,7 @@ public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock( "pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(4f).requiresTool()))
 
-
+//gg
 
 
 
