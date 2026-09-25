1a4d34677a8f9ba9039ce6361871012e8cb4c61d
gutierrezgamesnow <gutierrez.games.now@gmail.com>
2026-09-25 08:25:50 -0500

    added two new blocks

--- src/main/java/net/grantcode/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/grantcode/tutorialmod/block/ModBlocks.java
@@ -1,22 +1,31 @@
 package net.grantcode.tutorialmod.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.grantcode.tutorialmod.TutorialMod;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
     public static final Block PINK_GARNET_BLOCK = registerBlock( "pink_garnet_block",
-            new Block(AbstractBlock.Settings.create().strength(4f).requiresTool()))
+            new Block(AbstractBlock.Settings.create()
+                    .strength(4f)
+                    .requiresTool()
+                    .sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
 //gg
 
-
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
+            new Block(AbstractBlock.Settings.create()
+                    .strength(3f)
+                    .requiresTool()));
 
 
 
@@ -39,6 +48,13 @@ public class ModBlocks {
 
     public static void registerModBlocks(){
         TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.PINK_GARNET_BLOCK);
+            fabricItemGroupEntries.add(ModBlocks.RAW_PINK_GARNET_BLOCK);
+        });
+
+
 
 
     }
